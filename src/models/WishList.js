fad273f66b713a86b579b4ebb33e8b9f0cf16024
koko1313 <kaloyanvellichkov@gmail.com>
2020-05-20 13:07:51 +0300

    added models

--- src/models/WishList.js
+++ src/models/WishList.js
@@ -1,11 +1,30 @@
 import { types } from "mobx-state-tree";
 
-export const WishListItem = types.model({
-    name: types.string,
-    price: types.number,
-});
+export const WishListItem = types
+    .model({
+        name: types.string,
+        price: types.number,
+    }).actions(self => ({
+        changeName(newName) {
+            self.name = newName;
+        },
+        changePrice(newPrice) {
+            self.price = newPrice;
+        }
+    }));
 
-export const WishList = types.model({
-    // it's optional and if it's not provider, it's empty array
-    items: types.optional(types.array(WishListItem), [])
-});
+export const WishList = types
+    .model({
+        // it's optional and if it's not provider, it's empty array
+        items: types.optional(types.array(WishListItem), []),
+    })
+    .actions(self => ({
+        add(item) {
+            self.items.push(item);
+        }
+    }))
+    .views(self => ({
+        get totalPrice() {
+            return self.items.reduce((sum, entry) => sum + entry.price, 0);
+        }
+    }));
